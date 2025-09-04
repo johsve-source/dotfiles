91490de3c537ab91f69da226154b6b4840a130ee
Johan <22452724+johsve-source@users.noreply.github.com>
2025-09-04 12:47:53 +0200

    update

--- workspace/github.com/johsve-source/nvim-nix/lua/johsve-source/plugins/yazi.lua
+++ workspace/github.com/johsve-source/nvim-nix/lua/johsve-source/plugins/yazi.lua
@@ -1,6 +1,6 @@
 return {
 	"mikavilpas/yazi.nvim",
 	event = "VeryLazy",
-	keys = { { "<leader>fY", "<cmd>Yazi cwd<cr>", desc = "Open Yazi (cwd)" } },
+	keys = { { "<leader>fy", "<cmd>Yazi cwd<cr>", desc = "Open Yazi (cwd)" } },
 	opts = { open_for_directories = false },
 }
