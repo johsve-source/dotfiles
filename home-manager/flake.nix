3fb13beec55e1fe4e0095cd4bcd203e37028f6b9
Johan <22452724+johsve-source@users.noreply.github.com>
2025-09-05 08:55:13 +0200

    Use flake-inputs for nvim and tmux; add modules

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,5 +1,4 @@
-{
-  description = "Home Manager configuration of johan";
+{ description = "Home Manager configuration of johan";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,19 +6,35 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nvimNix = {
+      url = "github:johsve-source/nvim-nix";
+    };
+    tmuxFlake = {
+      url = "github:johsve-source/tmux";
+    };
   };
 
-  outputs =
-    { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nvimNix, tmuxFlake, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (nvimNix.overlay or (nvimNix.overlays or {}).default)
+          (tmuxFlake.overlay or (tmuxFlake.overlays or {}).default)
+        ];
+      };
     in
     {
       homeConfigurations."johan" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        extraSpecialArgs = { inherit self nvimNix tmuxFlake; };
 
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+          ./modules/nvim.nix
+          ./modules/tmux.nix
+        ];
       };
     };
 }
